9ddd0be2b338cc1103d8563516d050a56bd8f6a8
NikhilVamsiGrandhi <142904916+NikhilVamsiGrandhi@users.noreply.github.com>
2023-08-28 17:30:42 +0530

    Add files via upload

---
+++ string operations.py
@@ -0,0 +1,59 @@
+#strings length
+text = "Hello, World!"
+length = len(text)
+uppercase = text.upper()
+lowercase = text.lower()
+print(uppercase)
+print(length)
+
+
+#string operations
+'''
+str = "Hello"
+str1 = " world"
+print(str*3) # prints HelloHelloHello
+print(str+str1)# prints Hello world
+print(str[4]) # prints o
+print(str[2:4]); # prints ll
+print('w' in str) # prints false as w is not present in str
+print('wo' not in str1) # prints false as wo is present in str1.'''
+
+#diamond
+'''
+n=int(input("Enter a number: "))
+
+for i in range(1,n+1):
+    left_spaces=" "*(n-i)
+    middle_s=" "*((i-1)*2)
+    print(left_spaces+"/"+middle_s+"\\")
+for i in range(1,n+1):
+    left_spaces=" "*(i-1)
+    middle_s=" "*((n*2)-(i*2))
+    print(left_spaces+"\\"+middle_s+"/")'''
+
+
+#String Manipulation with reversed and with out it
+
+#1
+'''
+sentence = "Python is an amazing language"
+sentence=sentence.split()
+reversed_sentence = ' '.join(sentence[::-1])
+print(reversed_sentence)
+
+#2
+sentence = "Python is an amazing language"
+# Reverse the sentence
+reversed_sentence = ' '.join(reversed(sentence.split()))
+print(reversed_sentence)'''
+
+#re module (uses)
+
+import re
+
+text = "Email me at alice@example.com or bob@example.net"
+emails = re.findall(r'\S+@\S+', text)
+print(emails)
+
+
+
