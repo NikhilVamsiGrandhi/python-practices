033ead4ddf6364b293f8b059bec368af31d754ba
NikhilVamsiGrandhi <142904916+NikhilVamsiGrandhi@users.noreply.github.com>
2023-08-28 17:28:58 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,31 @@
+#artihmetic operation
+x = 10
+y= 5
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+
+
+#Comparison Operators
+is_equal = x == y
+is_not_equal = x != y
+greater_than = x > y
+less_than = x < y
+
+print(is_equal)
+print(is_not_equal) #gives true or false
+
+# Logical Operators
+
+is_true = True
+is_false = False
+logical_and = is_true and is_false
+logical_or = is_true or is_false
+logical_not = not is_true
+
+print(logical_not) #gives true or false
