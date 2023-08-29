cfd4b00827233b59c2a3fb4433b95111325bee2c
NikhilVamsiGrandhi <142904916+NikhilVamsiGrandhi@users.noreply.github.com>
2023-08-29 13:30:52 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,32 @@
+#count of lower_case and upper_case
+
+def count_of_lowercase_and_uppercase_letters(arg_1):
+    count_of_uppercase=0
+    count_of_lowercase=0
+    for i in arg_1:
+        if i==i.upper():
+            count_of_uppercase+=1
+        else:
+            count_of_lowercase+=1
+
+    print(count_of_uppercase)
+    print(count_of_lowercase)
+
+
+#longest_increasing_subsequence
+def longest_increasing_subsequence(sequence):
+    list_num = []
+    num = sequence[0]
+    list_num.append(num)
+
+    for i in range(len(sequence)):
+        for j in (sequence[i:]):
+            if num < j:
+                list_num.append(j)
+                num = j
+                break
+    return list_num
+
+
+sequence = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print(longest_increasing_subsequence(sequence))
